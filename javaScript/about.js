939a7281bef10792f1d7821df2407daa1f7b863b
Abhishekkumar990 <abhishekkumar.sharma.cs.2020@mitmeerut.ac.in>
2025-04-04 19:35:01 +0530

    DSD

--- javaScript/about.js
+++ javaScript/about.js
@@ -1,4 +1,6 @@
-function toggleMenu() {
-    const navLinks = document.querySelector('.nav-links');
-    navLinks.classList.toggle('active');
-}
+ const hamburger = document.getElementById('hamburger');
+ const navLinks = document.getElementById('navLinks');
+
+ hamburger.addEventListener('click', () => {
+     navLinks.classList.toggle('active');
+ });
