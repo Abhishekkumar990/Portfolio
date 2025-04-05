562ed14ddd74f42db687d6bf4e0de83c9b999f7c
Abhishekkumar990 <abhishekkumar.sharma.cs.2020@mitmeerut.ac.in>
2025-04-05 14:24:10 +0530

    css change

--- javaScript/index.js
+++ javaScript/index.js
@@ -3,3 +3,22 @@ function toggleMenu() {
     const navLinks = document.querySelector(".nav-links");
     navLinks.classList.toggle("show");
 }
+let inactivityTime = function() {
+    let timer;
+
+    function resetTimer() {
+        clearTimeout(timer);
+        timer = setTimeout(() => {
+            location.reload(); // refresh the page
+        }, 100000); // 2 minutes = 120000 milliseconds
+    }
+
+    // Events that reset the timer
+    window.onload = resetTimer;
+    document.onmousemove = resetTimer;
+    document.onkeypress = resetTimer;
+    document.onscroll = resetTimer;
+    document.onclick = resetTimer;
+};
+
+inactivityTime();
